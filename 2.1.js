9d389e41e64e0327c1e6adeeb0cd2f73f820a938
JasonA-95 <aucouturierjason@gmail.com>
2025-11-19 16:36:11 +0100

    Ajout final de tous mes fichiers JS

---
+++ 2.1.js
@@ -0,0 +1,9 @@
+let age = 20;
+
+if (age >= 18) {
+  console.log("You probably are an adult");
+} else if (age >= 1 && age < 18) {
+  console.log("You probably are a child");
+} else {
+  console.log("You probably are a baby");
+}
