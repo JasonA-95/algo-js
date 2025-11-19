a469e322b0fe1f74460a3608259c7374b2801a33
JasonA-95 <aucouturierjason@gmail.com>
2025-11-19 16:34:22 +0100

    Ajout final de tous mes fichiers JS

---
+++ 2.2.js
@@ -0,0 +1,12 @@
+let valeurmin = 50;
+let valeurmax = 100;
+let valeurutilisateur = 75;
+
+if (valeurutilisateur > valeurmax) {
+    console.log("BONUS");
+} else if (valeurutilisateur < valeurmin) {
+    console.log("RETOUR A LA CASE DEPART");
+} else {
+    console.log("CONTINUEZ L'AVENTURE");
+}
+
