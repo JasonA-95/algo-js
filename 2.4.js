a3751c4983bc1950845fff1caca9c01d09f0bdcf
JasonA-95 <aucouturierjason@gmail.com>
2025-11-20 14:25:58 +0100

    modifiction des exercices

--- 2.4.js
+++ 2.4.js
@@ -1,11 +1,11 @@
 let i = 1;
 
 while (i <= 100) {
-    if (i % 2 === 0) {   // i est pair
-        console.log(i / 2); 
-    } else {             // i est impair
+    if (i % 2 === 0) {
+        console.log(i / 2);
+    } else {
         console.log(i * 3);
     }
 
-    i += 1; // passer au nombre suivant
+    i++;
 }
